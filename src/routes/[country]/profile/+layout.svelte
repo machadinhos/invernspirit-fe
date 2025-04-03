dcd7030af9f468ba1d1982307ff68add84aa086a
machadinhos <pbmachado2002@gmail.com>
2025-04-03 10:54:51 +0100

    make forgot password verification code be 8 digits long

---
+++ src/routes/[country]/profile/+layout.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import { config, user } from '$state';
+  import { FaAddressCard, FaSolidReceipt } from 'svelte-icons-pack/fa';
+  import { Icon, type IconType } from 'svelte-icons-pack';
+  import { goto } from '$app/navigation';
+  import { onMount } from 'svelte';
+  import { page } from '$app/state';
+  import { profile } from '$content';
+
+  type Props = {
+    children: import('svelte').Snippet;
+  };
+
+  let { children }: Props = $props();
+
+  const selected = $derived(page.url.pathname.split('/')[3]);
+
+  onMount(() => {
+    config.afterInitialization(() => {
+      if (!user.isLoggedIn) goto(`/${page.params.country}`);
+    });
+  });
+</script>
+
+<svelte:head><title>{profile.headTitle}</title></svelte:head>
+
+{#snippet listItem(subPage: string, text: string, icon: IconType)}
+  <li class={subPage === selected ? 'bg-background' : 'opacity-50'}>
+    <a class="flex w-full items-center gap-2 px-3 py-1" href="/{page.params.country}/profile/{subPage}">
+      <Icon src={icon} />
+      {text}</a
+    >
+  </li>
+{/snippet}
+
+<div class="flex h-full w-full flex-col items-center">
+  <h1 class="mb-9 text-center text-5xl">{profile.title}</h1>
+  <div class="flex w-[90%] max-w-[900px] gap-5">
+    <ul class="flex flex-col gap-2">
+      {@render listItem('user-details', profile.links.userDetails, FaAddressCard)}
+      {@render listItem('orders', profile.links.orders, FaSolidReceipt)}
+    </ul>
+    {@render children()}
+  </div>
+</div>
