a2437a6b106dd1821668c49ace75788ee860ce9a
machadinhos <pbmachado2002@gmail.com>
2025-04-05 20:30:28 +0100

    add referer for direct be calls

--- src/lib/server/service/be-client.ts
+++ src/lib/server/service/be-client.ts
@@ -1,4 +1,4 @@
-import { BE_HOST, BE_ID_KEY, BE_ID_VALUE, BE_SECRET_KEY, BE_SECRET_VALUE } from '$env/static/private';
+import { BE_HOST, BE_ID_KEY, BE_ID_VALUE, BE_SECRET_KEY, BE_SECRET_VALUE, FE_HOST } from '$env/static/private';
 import { prepareGetAllCollections, prepareGetCollectionById } from '$lib/service/endpoints/collections';
 import { prepareGetAllProducts, prepareGetProductById } from '$lib/service/endpoints/products';
 import { prepareGetCountries } from '$lib/service/endpoints/countries';
@@ -7,6 +7,7 @@ import type { RequestHostContext } from '$lib/service/client/client';
 const headers = {
   [BE_ID_KEY]: BE_ID_VALUE,
   [BE_SECRET_KEY]: BE_SECRET_VALUE,
+  Referer: FE_HOST,
 };
 
 const context: RequestHostContext = {
