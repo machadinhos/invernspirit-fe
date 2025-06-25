dc149afb6ca5189d8e3a0377012eaac979e31866
machadinhos <pbmachado2002@gmail.com>
2025-06-25 12:08:48 +0100

    feat(vite): add vite-plugin-devtools-json support
    Add `vite-plugin-devtools-json` to configure the new devtools features.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,23 @@
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(() => {
-  return {
-    plugins: [sveltekit(), tailwindcss()],
+export default defineConfig({
+  plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
 
-    server: {
-      allowedHosts: ['.invernspirit.com'],
-      proxy: {
-        '/api': {
-          target: 'http://localhost:9000',
-          changeOrigin: true,
-        },
-        '/stock': {
-          target: 'http://localhost:8788',
-          rewrite: (path): string => path.replace(/^\/stock/, '/private/stock'),
-          changeOrigin: true,
-        },
+  server: {
+    allowedHosts: ['.invernspirit.com'],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9000',
+        changeOrigin: true,
+      },
+      '/stock': {
+        target: 'http://localhost:8788',
+        rewrite: (path): string => path.replace(/^\/stock/, '/private/stock'),
+        changeOrigin: true,
       },
     },
-  };
+  },
 });
