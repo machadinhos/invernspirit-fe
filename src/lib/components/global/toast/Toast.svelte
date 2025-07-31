b870378b26099656f23cfc8da1a9cbe09c84114b
machadinhos <pbmachado2002@gmail.com>
2025-07-31 22:28:29 +0100

    refactor(toast): simplify event implementation
    Only use pointer events.

--- src/lib/components/global/toast/Toast.svelte
+++ src/lib/components/global/toast/Toast.svelte
@@ -2,10 +2,8 @@
   import { type Component, onMount } from 'svelte';
   /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
   import { fade, fly } from 'svelte/transition';
-  import type { Attachment } from 'svelte/attachments';
   import { FaSolidXmark } from 'svelte-icons-pack/fa';
   import { Icon } from 'svelte-icons-pack';
-  import { on } from 'svelte/events';
   import { type Toast } from '$state';
 
   type Props = {
@@ -30,69 +28,49 @@
     toast.progress = (effectiveRemaining / (toast.duration ?? 0)) * 100;
   };
 
-  const getClientX = (e: MouseEvent | TouchEvent): number => {
-    return 'clientX' in e ? e.clientX : e.touches[0].clientX;
-  };
-
-  const startDrag = (e: MouseEvent | TouchEvent): void => {
-    startX = getClientX(e);
-    dragging = true;
+  const onpointerdown = (e: PointerEvent): void => {
+    if (dragging) return;
+    startX = e.clientX;
+    currentX = 0;
     outerElement.style.transition = 'none';
+    outerElement.setPointerCapture(e.pointerId);
+    dragging = true;
     toast.pauseTimer();
   };
 
-  const dragMove = (e: MouseEvent | TouchEvent): void => {
+  const onpointermove = (e: PointerEvent): void => {
     if (!dragging || startX === undefined) return;
-    currentX = getClientX(e) - startX;
+    currentX = e.clientX - startX;
     if (Math.abs(currentX) < 130) {
       outerElement.style.transform = `translateX(${currentX}px)`;
     }
   };
 
-  const endDrag = (e: MouseEvent | TouchEvent): void => {
-    dragging = false;
+  const onpointerup = (e: PointerEvent): void => {
+    if (!dragging) return;
     outerElement.style.transition = '';
     if (Math.abs(currentX) > 65) {
       toast.destroy();
     } else {
+      if (e.pointerType === 'mouse' && Math.abs(currentX) <= 1) {
+        const target = document.elementFromPoint(e.clientX, e.clientY);
+        if (target && target !== outerElement) {
+          target.dispatchEvent(new PointerEvent('click', e));
+        }
+      }
       outerElement.style.transform = '';
-      if ('touches' in e && Math.abs(currentX) > 1) {
+      if (e.pointerType === 'touch' && Math.abs(currentX) > 1) {
         toast.startTimer();
       }
-      currentX = 0;
     }
+    dragging = false;
   };
 
-  const onpointerdown = (e: PointerEvent): void => {
-    outerElement.setPointerCapture(e.pointerId);
-  };
-
-  const onpointerup = (e: PointerEvent): void => {
-    outerElement.releasePointerCapture(e.pointerId);
-    if (Math.abs(currentX) <= 1) {
-      const clickedElement = document.elementFromPoint(e.clientX, e.clientY);
-      if (clickedElement && clickedElement !== outerElement) {
-        clickedElement.dispatchEvent(
-          new MouseEvent('click', {
-            bubbles: true,
-            cancelable: true,
-            view: window,
-          }),
-        );
-      }
-    }
-  };
-
-  const attach: Attachment<HTMLDivElement> = (node: HTMLDivElement) => {
-    const touchstartCleanup = on<HTMLDivElement, 'touchstart'>(node, 'touchstart', startDrag, { passive: false });
-    const touchmoveCleanup = on<HTMLDivElement, 'touchmove'>(node, 'touchmove', dragMove, { passive: false });
-    const touchendCleanup = on<HTMLDivElement, 'touchend'>(node, 'touchend', endDrag, { passive: false });
-
-    return (): void => {
-      touchstartCleanup();
-      touchmoveCleanup();
-      touchendCleanup();
-    };
+  const onpointercancel = (e: PointerEvent): void => {
+    dragging = false;
+    outerElement.style.transition = '';
+    outerElement.style.transform = '';
+    if (e.pointerType === 'touch') toast.startTimer();
   };
 
   onMount(() => {
@@ -109,17 +87,15 @@
 <div
   bind:this={outerElement}
   class={[
-    'toast relative pt-2 pr-2 pb-2 select-none',
+    'toast relative touch-pan-y pt-2 pr-2 pb-2 select-none',
     toast.type === 'normal' ? 'bg-background' : toast.type === 'error' ? 'bg-error' : 'bg-success',
   ]}
-  {@attach attach}
   aria-live={toast.type === 'error' ? 'assertive' : 'polite'}
-  onmousedown={startDrag}
   onmouseenter={toast.pauseTimeOnHover ? toast.pauseTimer : undefined}
   onmouseleave={toast.pauseTimeOnHover ? toast.startTimer : undefined}
-  onmousemove={dragMove}
-  onmouseup={endDrag}
+  {onpointercancel}
   {onpointerdown}
+  {onpointermove}
   {onpointerup}
   role={toast.type === 'error' ? 'alert' : 'status'}
   transition:fly|fade={{ y: 50, duration: 500 }}
