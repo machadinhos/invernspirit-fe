7fe7328873cc5ab95cd8f49338853e4cdaeb2e4b
machadinhos <pbmachado2002@gmail.com>
2025-09-03 09:32:14 +0100

    refactor: simplify shop layout load function

--- src/routes/[country]/(shopfront)/shop/+layout.server.ts
+++ src/routes/[country]/(shopfront)/shop/+layout.server.ts
@@ -1,16 +1,12 @@
 import { beClient } from '$service-server';
-import { building } from '$app/environment';
 import type { Collection } from '$types';
 import type { LayoutServerLoad } from './$types';
 
 const cachedData: Record<string, { collections: Collection[] }> = {};
 
 export const load: LayoutServerLoad = async ({ params }) => {
-  if (building) {
-    if (!cachedData[params.country]) {
-      cachedData[params.country] = { collections: await beClient.collections.getAll(params.country) };
-    }
-    return cachedData[params.country];
+  if (!cachedData[params.country]) {
+    cachedData[params.country] = { collections: await beClient.collections.getAll(params.country) };
   }
-  return { collections: await beClient.collections.getAll(params.country) };
+  return cachedData[params.country];
 };
