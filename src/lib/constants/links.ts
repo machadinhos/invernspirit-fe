ca40bb0692c633c56369d695516dcce12428f626
machadinhos <pbmachado2002@gmail.com>
2025-09-12 16:47:03 +0100

    perf: use avif images for big images
    avif produces much smaller files, so even doe it has a higher decoding
    cost the much faster download should make it faster overall.

--- src/lib/constants/links.ts
+++ src/lib/constants/links.ts
@@ -1,15 +1,15 @@
 export const imageLinks = {
   home: {
     bgImages: [
-      'https://images.invernspirit.com/products/ceramics-product-1-1.webp',
-      'https://images.invernspirit.com/products/ceramics-product-2-1.webp',
-      'https://images.invernspirit.com/products/ceramics-product-3-1.webp',
-      'https://images.invernspirit.com/products/ceramics-product-4-1.webp',
+      'https://images.invernspirit.com/products/ceramics-product-1-1.avif',
+      'https://images.invernspirit.com/products/ceramics-product-2-1.avif',
+      'https://images.invernspirit.com/products/ceramics-product-3-1.avif',
+      'https://images.invernspirit.com/products/ceramics-product-4-1.avif',
     ],
   },
   about: {
-    banner: 'https://images.invernspirit.com/about/about-1.webp',
-    mafalda: 'https://images.invernspirit.com/about/about-2.webp',
+    banner: 'https://images.invernspirit.com/about/about-1.avif',
+    mafalda: 'https://images.invernspirit.com/about/about-2.avif',
   },
 };
 
